dc17f0fb847269a2dd786f2dc6571ffcff4990d4
wpeguero <wpeguerorosario@outlook.com>
2022-05-20 23:16:08 -0400

    Fix(bug): Autocompletion tab error
    Used the suggested and best minimal setup for cmp. Now neovim is able to
    properly tab in the case where there are no suggestions popping up on
    the screen

--- lua/acconfig.lua
+++ lua/acconfig.lua
@@ -1,14 +1,17 @@
+-- ...
 local lspkind = require('lspkind')
 local cmp = require("cmp")
+local luasnip = require("luasnip")
 
-local has_words_before = function()
-	local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-local feedkey = function(key, mode)
-	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end
+--local has_words_before = function()
+--  local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+--  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+--end
+--
+--local feedkey = function(key, mode)
+--  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+--end
+-- ...
 
 -- ...
 
@@ -38,6 +41,7 @@ cmp.setup({
 		{ name = 'nvim_lsp' },
 		{ name = 'vsnip' },
 		{ name = 'buffer' },
+		{ name = 'luasnip' },
 	},
 	-- ...
 })
@@ -59,10 +63,8 @@ cmp.setup({
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-			elseif vim.fn["vsnip#available"](1) == 1 then
-				feedkey("<Plug>(vsnip-expand-or-jump)", "")
-			elseif has_words_before() then
-				cmp.complete()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
 			else
 				fallback()
 			end
@@ -71,8 +73,10 @@ cmp.setup({
 		["<S-Tab>"] = cmp.mapping(function()
 			if cmp.visible() then
 				cmp.select_prev_item()
-			elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-				feedkey("<Plug>(vsnip-jump-prev)", "")
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
 			end
 		end, { "i", "s" }),
 	},
