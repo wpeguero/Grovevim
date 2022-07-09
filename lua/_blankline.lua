50367687a660d04f22e6706e05e4e9b8b514549a
wpeguero <wpeguerorosario@outlook.com>
2022-07-09 08:33:25 -0400

    MINOR CHANGE: Removed underline at beginning.

--- lua/_blankline.lua
+++ lua/_blankline.lua
@@ -22,7 +22,7 @@ vim.opt.list = true
 
 require("indent_blankline").setup{
 	show_current_context = true,
-	show_current_context_start = true,
+	show_current_context_start = false,
 	space_char_blankline = " ",
 	char_highlight_list = {
 		"IndentBlanklineIndent1",
