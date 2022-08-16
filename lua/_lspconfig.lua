873c6ad4bb99a7acf3b58852c7a203aa86f4c183
wpeguero <wpeguerorosario@outlook.com>
2022-08-16 08:33:44 -0400

    MINOR CHANGE: removed global variable vim

--- lua/_lspconfig.lua
+++ lua/_lspconfig.lua
@@ -126,13 +126,13 @@ require 'lspconfig'.sumneko_lua.setup {
 		Lua = {
 			runtime = {
 				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-				version = 'LuaJIT',
+				version = 'LuaJIT 2.1',
 				-- Setup your lua path
 				path = runtime_path,
 			},
 			diagnostics = {
 				-- Get the language server to recognize the `vim` global
-				globals = { 'vim' },
+				-- globals = { 'vim' },
 			},
 			workspace = {
 				-- Make the server aware of Neovim runtime files
