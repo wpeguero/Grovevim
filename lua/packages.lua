6bf1c6535c8482d7fbb1459727f8aa4bff2cded6
wpeguero <wpeguerorosario@outlook.com>
2022-10-25 13:56:31 -0400

    NEW:Added bootstrapping to the packages file.
    Updated the packages.lua file with the latest packer.nvim configurations

--- lua/packages.lua
+++ lua/packages.lua
@@ -1,6 +1,19 @@
 -- START
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 Use = require("packer").use
-require("packer").startup(function()
+require("packer").startup(function(Use)
 	-- Package Manager
 	Use 'wbthomason/packer.nvim'
 	-- ...
@@ -82,6 +95,11 @@ require("packer").startup(function()
 	-- ...
 	-- Indent lines
 	Use {"lukas-reineke/indent-blankline.nvim"}
+
+	if packer_bootstrap then
+		require('packer').sync()
+	end
+
 end)
 
 -- END
